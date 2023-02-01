b41ba8d1e8ddd13d924c15b60efd24d0611fd8aa
Bill Carioca <billcarioca@hotmail.com>
2023-02-01 12:24:26 -0300

    Category

---
+++ src/main/java/com/billcarioca/course/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.billcarioca.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.billcarioca.course.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long>{
+
+}
