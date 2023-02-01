97e47fb3088308af2a8da4f1ee48fe8f63dbbb59
Bill Carioca <billcarioca@hotmail.com>
2023-02-01 08:53:50 -0300

    Service layer, component registration

---
+++ src/main/java/com/billcarioca/course/services/UserService.java
@@ -0,0 +1,26 @@
+package com.billcarioca.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.billcarioca.course.entities.User;
+import com.billcarioca.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
